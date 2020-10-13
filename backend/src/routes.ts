579c1c853aec521e295d5b822d2f469f5dbb3871
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-10-13 16:19:43 -0300

    feat(api): add image upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import OrphanageController from "./controllers/orphanages-controller";
+import uploadConfig from "./config/uploads";
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post("/orphanages", OrphanageController.create);
+routes.post("/orphanages", upload.array("images"), OrphanageController.create);
 routes.get("/orphanages", OrphanageController.index);
 routes.get("/orphanages/:id", OrphanageController.show);
 

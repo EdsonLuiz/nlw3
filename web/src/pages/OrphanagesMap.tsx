008ce7404ecf5f21758381f4ca232865b52872fa
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-10-14 11:16:17 -0300

    feat(web): add maps, navigation, orphanage create and show pages

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { FiPlus } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
-import {Map, TileLayer} from 'react-leaflet'
+import React from "react";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
-import '../styles/pages/orphanages-map.css'
-import 'leaflet/dist/leaflet.css'
+import "../styles/pages/orphanages-map.css";
+import "leaflet/dist/leaflet.css";
 
-import mapMarkerImg from '../../src/images/map-marker.svg'
+import mapMarkerImg from "../../src/images/map-marker.svg";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarkerImg} alt="marcador de mapa happy"/>
+          <img src={mapMarkerImg} alt="marcador de mapa happy" />
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças estão esperando a sua visita :)</p>
         </header>
@@ -25,19 +33,32 @@ function OrphanagesMap() {
       </aside>
 
       <Map
-        center={[-30.0056576,-51.1574016]}
+        center={[-30.0056576, -51.1574016]}
         zoom={15}
-        style={{width: '100%', height: '100%'}}
+        style={{ width: "100%", height: "100%" }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <Marker icon={mapIcon} position={[-30.0056576, -51.1574016]}>
+          <Popup
+            className="map-popup"
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+          >
+            Some orphanage
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
-
     </div>
-  )
+  );
 }
 
-export default OrphanagesMap
+export default OrphanagesMap;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
-import OrphanagesMap from './pages/OrphanagesMap'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import OrphanagesMap from "./pages/OrphanagesMap";
+import CreateOrphanage from "./pages/CreateOrphanage";
+import Orphanage from "./pages/Orphanage";
 
 function Routes() {
   return (
@@ -9,9 +11,12 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Routes
+export default Routes;

1c1801ab90f662685d3d4c486452e1eae54dd31a
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-10-15 11:08:30 -0300

    feat(mobile): maps page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,53 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import {
+  Container,
+  MapContainer,
+  CalloutContainer,
+  CalloutText,
+  FooterContainer,
+  FooterText,
+  CreateOrphanageButton,
+  CreateOrphanageButtonText,
+} from "./styles";
+
+import mapMarker from "./src/images/map-marker.png";
+import { Marker } from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <MapContainer
+        initialRegion={{
+          latitude: -29.9983735,
+          longitude: -51.1009617,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -29.9983735,
+            longitude: -51.1009617,
+          }}
+        >
+          <CalloutContainer onPress={() => {}} tooltip>
+            <CalloutText>Lar das meninas </CalloutText>
+          </CalloutContainer>
+        </Marker>
+      </MapContainer>
+
+      <FooterContainer>
+        <FooterText>Dois orfanatos encontrados</FooterText>
+        <CreateOrphanageButton onPress={() => {}}>
+          <Feather name="plus" size={20} color="#fff" />
+        </CreateOrphanageButton>
+      </FooterContainer>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ mobile/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components/native";
+import MapView, { Callout } from "react-native-maps";
+import { Dimensions, TouchableOpacity } from "react-native";
+
+export const Container = styled.View``;
+
+export const MapContainer = styled(MapView)`
+  width: ${Dimensions.get("window").width}px;
+  height: ${Dimensions.get("window").height}px;
+`;
+
+export const CalloutContainer = styled(Callout)`
+  width: 160px;
+  height: 46px;
+  padding: 0 16px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 16px;
+  justify-content: center;
+`;
+
+export const CalloutText = styled.Text`
+  color: #0089a5;
+  font-size: 14px;
+`;
+
+export const FooterContainer = styled.View`
+  position: absolute;
+  left: 24px;
+  right: 24px;
+  bottom: 32px;
+
+  background-color: #ddd;
+  border-radius: 20px;
+  height: 56px;
+  padding-left: 24px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  elevation: 3;
+`;
+
+export const FooterText = styled.Text`
+  color: #8fa8b3;
+`;
+
+export const CreateOrphanageButton = styled(TouchableOpacity)`
+  width: 56px;
+  height: 54px;
+  background-color: #15c3d6;
+  border-radius: 20px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CreateOrphanageButtonText = styled.Text``;
